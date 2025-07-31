7ff3a7da4154a1d7d8dc8d28e6041b6d15350d55
Raunik Rai <raunik.rai@amptech.corp>
2025-08-01 00:19:18 +0530

    changed cors origin

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: '*', // replace with your frontend URL
+    origin: 'https://www.mitronwear.com', // replace with your frontend URL
 
     credentials: true
 }));
